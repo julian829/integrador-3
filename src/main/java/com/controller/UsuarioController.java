e40261d93353fa6250461fa30e040c97c79ca5af
julian829 <juliangiraldobravo@gmail.com>
2026-05-24 12:11:34 -0500

    Backend inicial Spring Boot - Julian

---
+++ src/main/java/com/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.Usuario;
+import com.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return (List<Usuario>) usuarioService.listarUsuarios();
+    }
+}
